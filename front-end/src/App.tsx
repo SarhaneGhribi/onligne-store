cec84d0906c9386e58458f5a989e35eeb1fe5a5a
Sarhane Ghribi <sarhane.ghribi@maraboutcorp.com>
2024-05-31 11:19:06 +0100

    navbar done

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import PrivateRoute from "./components/highOrderComponents/PrivateRoute";
-import _NavbarLayout from "./components/highOrderComponents/_NavbarLayout";
+import NavbarLayout from "./components/highOrderComponents/NavbarLayout";
 import SignIn from "./pages/signIn/SignIn";
 import SignUp from "./pages/signUp/SignUp";
 import Home from "./pages/home/Home";
@@ -9,9 +8,15 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<_NavbarLayout children={<Home/>}/>}/>
-        <Route path="/SignUp" element={<_NavbarLayout children={<SignUp />}/>} />
-        <Route path="/SignIn" element={<_NavbarLayout children={<SignIn />}/>} />
+        <Route path="/" element={<NavbarLayout children={<Home />} />} />
+        <Route
+          path="/SignUp"
+          element={<NavbarLayout children={<SignUp />} />}
+        />
+        <Route
+          path="/SignIn"
+          element={<NavbarLayout children={<SignIn />} />}
+        />
       </Routes>
     </BrowserRouter>
   );
