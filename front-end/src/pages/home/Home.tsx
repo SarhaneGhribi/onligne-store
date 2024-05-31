cd4cad229d69323de02d14e1061b36b6fb18aaef
Sarhane Ghribi <sarhane.ghribi@maraboutcorp.com>
2024-05-31 15:11:43 +0100

    Product card

--- front-end/src/pages/home/Home.tsx
+++ front-end/src/pages/home/Home.tsx
@@ -1,8 +1,20 @@
 import React from "react";
 import "./Home.css";
 import Categories from "./components/Categories";
-
+import { Product } from "../../types";
+import productImage from "../../assets/images/image-removebg-preview(1).png";
+import ProductCard from "../../components/costumComponents/cards/productCard/ProductCard";
 type Props = {};
+
+const product: Product = {
+  id: 1,
+  name: "Man Hoodie",
+  price: 20.99,
+  description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  image: productImage,
+  categoryId: 1,
+  subCategoryId: 1,
+};
 
 const Home = (props: Props) => {
   return (
@@ -16,7 +28,9 @@ const Home = (props: Props) => {
           <div className="bottomRightContainer"></div>
         </div>
         <div className="leftContainer">
-          <div className="topLeftContainer"></div>
+          <div className="topLeftContainer">
+            <ProductCard product={product} />
+          </div>
           <div className="bottomLeftContainer"></div>
         </div>
       </div>
